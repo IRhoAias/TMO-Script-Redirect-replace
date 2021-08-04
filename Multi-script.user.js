57b28685da46a0b5aa3e94f700662bf32bbd6984
IRhoAias <88462069+IRhoAias@users.noreply.github.com>
2021-08-04 18:53:17 -0300

    Add files via upload

---
+++ Multi-script.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name Multi-script Para TMO
+// @namespace Mother Of Mangas 2021
+// @version 0.1
+// @description Quiero Ver Mi MANGA!!!!
+// @author RhoAias
+// @include *
+// @grant none
+// ==/UserScript==
+
+if (window.top !== window.self)	   // Don’t run in frames.
+	return;
+
+var currentURL = location.href;
+
+
+// ### cook2love.com/news...
+
+if (currentURL.match("cook2love.com/news")) {
+	location.href = location.href.replace("cook2love.com/news", "lectortmo.com/viewer");
+};
+
+
+// ### wtechnews.com/news...
+
+if (currentURL.match("wtechnews.com/news")) {
+	location.href = location.href.replace("wtechnews.com/news", "lectortmo.com/viewer");
+};
+
+
+// ### animationforyou.com/news...
+
+if (currentURL.match("animationforyou.com/news")) {
+	location.href = location.href.replace("animationforyou.com/news", "lectortmo.com/viewer");
+};
+
+
+// ### animalcanine.com/news...
+
+if (currentURL.match("animalcanine.com/news")) {
+	location.href = location.href.replace("animalcanine.com/news", "lectortmo.com/viewer");
+};
+
+
+// ### gamesnk.com/news...
+
+if (currentURL.match("gamesnk.com/news")) {
+	location.href = location.href.replace("gamesnk.com/news", "lectortmo.com/viewer");
+};
+
+
+// end of script
